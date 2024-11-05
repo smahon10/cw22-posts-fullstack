2809f050ae9faf70b82c359056104ed87d586be2
smahon10 <smahon10@jhu.edu>
2024-11-05 14:07:17 -0500

    Initial Commit

---
+++ api/src/app.ts
@@ -0,0 +1,25 @@
+import { Hono } from "hono";
+import postRoutes from "./routes/posts";
+import { HTTPException } from "hono/http-exception";
+import commentRoutes from "./routes/comments";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.route("/", postRoutes);
+app.route("/", commentRoutes);
+
+app.onError((err, c) => {
+  console.error(`${err}`);
+
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+
+  return c.json({ message: "An unexpected error occurred!" }, 500);
+});
+
+export default app;
